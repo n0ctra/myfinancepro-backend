215dabfd1db34a714a68ba865d386bd8a9c3d2f2
alej0nt666 <alej0nt666@gmail.com>
2025-06-14 00:35:48 -0500

    first commit

---
+++ src/main/java/com/api/myFinancePro/MyFinanceProApplication.java
@@ -0,0 +1,13 @@
+package com.api.myFinancePro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFinanceProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFinanceProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/myFinancePro/MyFinanceProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.myFinancePro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFinanceProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
